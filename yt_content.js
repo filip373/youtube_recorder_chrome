932f2f6e9085252865979cebf13ab0dfc5c1d98d
Filip <180691@edu.p.lodz.pl>
2016-10-30 11:25:25 +0100

    fixed loading video player

--- yt_content.js
+++ yt_content.js
@@ -1,22 +1,37 @@
 console.log("initialize content");
 
+var current = 0;
+
+var handlePlaying = function() {
+  current = Date.now();
+  chrome.runtime.sendMessage({action: "video", state: "play", time: current});
+  console.log("started playing");
+};
+
+var handlePaused = function() {
+  if(current > 0) {
+    chrome.runtime.sendMessage({action: "video", state: "pause", time: Date.now() - current});
+    console.log("paused");
+  }
+};
+
+var registerVideo = function(video) {
+  video.on("playing", handlePlaying).on("pause waiting", handlePaused);
+};
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "active") {
     sendResponse({active: true});
   }
 });
-
-var current = 0;
-
-$("video").on("playing", function() {
-  current = Date.now();
-	chrome.runtime.sendMessage({action: "video", state: "play", time: current});
-  console.log("started playing");
-});
-
-$("video").on("pause waiting", function() {
-	if(current > 0) {
-		chrome.runtime.sendMessage({action: "video", state: "pause", time: Date.now() - current});
-		console.log("paused");
-	}
-});
+  
+console.log("waiting for video player");
+var checkExist = setInterval(function() {
+  var video = $("video.html5-main-video");
+  if (video.length) {
+    console.log("video player loaded");
+    clearInterval(checkExist);
+    registerVideo(video);
+    handlePlaying();
+  }
+}, 100); // check every 100ms
