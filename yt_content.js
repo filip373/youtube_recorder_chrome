a64180b6104da1c91d62df9d4abbeb21ff68cc37
Filip <180691@edu.p.lodz.pl>
2016-10-30 10:10:26 +0100

    fixed indentation

--- yt_content.js
+++ yt_content.js
@@ -1,19 +1,19 @@
 console.log("initialize content");
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.action === "active") {
-		sendResponse({active: true});
-	}
+  if (request.action === "active") {
+    sendResponse({active: true});
+  }
 });
 
 var current = 0;
 
 $("video").on("playing", function() {
-    current = Date.now();
+  current = Date.now();
 	chrome.runtime.sendMessage({action: "video", state: "play", time: current});
-    console.log("started playing");
+  console.log("started playing");
 });
- 
+
 $("video").on("pause waiting", function() {
 	if(current > 0) {
 		chrome.runtime.sendMessage({action: "video", state: "pause", time: Date.now() - current});
